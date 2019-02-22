92070154c968cb1899286d7b57b64788efbff18f
Fabian Nitsch <f.nitsch@outlook.com>
2019-02-22 20:19:15 +0100

    Initial commit

---
+++ src/main/java/org/nitsch/sonar/plugins/SonarDroolsPlugin.java
@@ -0,0 +1,13 @@
+package org.nitsch.sonar.plugins;
+
+import org.sonar.api.Plugin;
+
+/**
+ * This class is the entry point for all extensions. It is referenced in pom.xml.
+ */
+public class SonarDroolsPlugin implements Plugin {
+
+    public void define(Context context) {
+
+    }
+}
